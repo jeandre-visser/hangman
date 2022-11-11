d93227505ffc127af97b2580ab5a00bbf5222d65
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-11 16:17:12 -0700

    Add divs and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from 'react';
 import words from './list.json';
+import './App.scss';
 
 function App() {
 
   const [guessWord, setGuessWord] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   });
-  console.log(guessWord)
+
+  const [letterGuess, setLetterGuess] = useState<string[]>([]);
+
+
   return (
-    <h1>Hangman</h1>
+    <div className="app__main">
+      <div className="app__header">
+        Lose Win
+      </div>
+      <Sketch />
+      <Word />
+      <Keyboard />
+    </div>
   )
 }
 
