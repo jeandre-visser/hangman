515868208367284f7f3a9bc2f26ea06e81a70911
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-11 16:51:05 -0700

    Create and style the head

--- src/components/Sketch/Sketch.tsx
+++ src/components/Sketch/Sketch.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
 import './Sketch.scss';
+
+const HEAD = (
+  <div>
+    <img className='app__head' src="src\assets\head.png" alt="head" />
+  </div>
+)
 
 const Sketch = () => {
   return (
     <div className="app__sketch">
+      {HEAD}
       <div className="app__drop-bar" />
       <div className="app__top-bar" />
       <div className="app__middle-bar" />
