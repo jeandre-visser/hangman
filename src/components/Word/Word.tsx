c83c58c58ddadb3e77b7d26aa52a67138521b52e
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-11 18:23:44 -0700

    Center letters and only visible when letter exists in word

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -3,11 +3,16 @@ import './Word.scss';
 import React from 'react'
 
 const Word = () => {
-  const word = 'test';
+  const word = 'show';
+  const guesses = ["s"];
   return (
     <div className="app__word">
       {word.split('').map((letter, index) => (
-        <span className="app__letters">{letter}</span>
+        <span className="app__letters" key={index}>
+          <span style={{ visibility: guesses.includes(letter) ? 'visible' : 'hidden' }}>
+            {letter}
+          </span>
+        </span>
       ))}
     </div>
   )
