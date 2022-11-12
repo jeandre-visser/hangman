b92b0d1cdcff8c636352bc62ea5d163fe5318a5d
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-11 17:40:31 -0700

    Add and style legs

--- src/components/Sketch/Sketch.tsx
+++ src/components/Sketch/Sketch.tsx
@@ -7,6 +7,8 @@ const Sketch = () => {
       <div className="app__torso" />
       <img className="app__right-arm" src="src\assets\right.png" alt="right-arm" />
       <img className="app__left-arm" src="src\assets\left.png" alt="left-arm" />
+      <img className="app__left-leg" src="src\assets\left-leg.png" alt="left-leg" />
+      <img className="app__right-leg" src="src\assets\right-leg.png" alt="right-leg" />
       <div className="app__drop-bar" />
       <div className="app__top-bar" />
       <div className="app__middle-bar" />
