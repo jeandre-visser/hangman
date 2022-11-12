1710d4fb6f433f8b827e64989d98bb842a8bd17c
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-11 17:10:31 -0700

    Add the arms

--- src/components/Sketch/Sketch.tsx
+++ src/components/Sketch/Sketch.tsx
@@ -5,6 +5,8 @@ const Sketch = () => {
     <div className="app__sketch">
       <img className='app__head' src="src\assets\head.png" alt="head" />
       <div className="app__torso" />
+      <img className="app__right-arm" src="src\assets\right.png" alt="right-arm" />
+      <img className="app__left-arm" src="src\assets\left.png" alt="left-arm" />
       <div className="app__drop-bar" />
       <div className="app__top-bar" />
       <div className="app__middle-bar" />
