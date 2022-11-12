ce831f22b3dfa5e3c8cf76048c0f76257afe4199
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-12 11:48:30 -0700

    File for body parts and import into Sketch.tsx

---
+++ src/components/Sketch/Body.tsx
@@ -0,0 +1,25 @@
+const HEAD = (
+  <img className='app__head' src="src\assets\head.png" alt="head" />
+);
+
+const TORSO = (
+  <div className="app__torso" />
+);
+
+const RIGHT_ARM = (
+  <img className="app__right-arm" src="src\assets\right.png" alt="right-arm" />
+)
+
+const LEFT_ARM = (
+  <img className="app__left-arm" src="src\assets\left.png" alt="left-arm" />
+)
+
+const RIGHT_LEG = (
+  <img className="app__right-leg" src="src\assets\right-leg.png" alt="right-leg" />
+)
+
+const LEFT_LEG = (
+  <img className="app__left-leg" src="src\assets\left-leg.png" alt="left-leg" />
+)
+
+export const BODY = [HEAD, TORSO, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
--- src/components/Sketch/Sketch.tsx
+++ src/components/Sketch/Sketch.tsx
@@ -1,4 +1,5 @@
 import './Sketch.scss';
+import { BODY } from './Body';
 
 type SketchProps = {
   numOfGuesses: number;
@@ -7,12 +8,7 @@ type SketchProps = {
 const Sketch = ({ numOfGuesses }: SketchProps) => {
   return (
     <div className="app__sketch">
-      <img className='app__head' src="src\assets\head.png" alt="head" />
-      <div className="app__torso" />
-      <img className="app__right-arm" src="src\assets\right.png" alt="right-arm" />
-      <img className="app__left-arm" src="src\assets\left.png" alt="left-arm" />
-      <img className="app__left-leg" src="src\assets\left-leg.png" alt="left-leg" />
-      <img className="app__right-leg" src="src\assets\right-leg.png" alt="right-leg" />
+      {BODY}
       <div className="app__drop-bar" />
       <div className="app__top-bar" />
       <div className="app__middle-bar" />
