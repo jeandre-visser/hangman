c10a1cdec63712c3c12a457b4e457bb9d0693196
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-13 15:31:40 -0700

    If word is revealed then show unguessed letters in red

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -3,15 +3,17 @@ import './Word.scss';
 type WordProps = {
   letterGuesses: string[];
   guessedWord: string;
+  showWord?: boolean
 }
 
-const Word = ({ guessedWord, letterGuesses }: WordProps) => {
+const Word = ({ guessedWord, letterGuesses, showWord = false }: WordProps) => {
 
   return (
     <div className="app__word">
       {guessedWord.split('').map((letter, index) => (
         <span className="app__letters" key={index}>
-          <span style={{ visibility: letterGuesses.includes(letter) ? 'visible' : 'hidden' }}>
+          <span style={{ visibility: letterGuesses.includes(letter) || showWord ? 'visible' : 'hidden',
+        color: !letterGuesses.includes(letter) && showWord ? "#ff2222" : "#000" }}>
             {letter}
           </span>
         </span>
